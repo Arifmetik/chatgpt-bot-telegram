25f0fb0f426d34344d4328d25e27a8e2ac17bbe5
Arifmetik <100046368+Arifmetik@users.noreply.github.com>
2023-02-03 17:10:57 +0500

    Update index.js

--- index.js
+++ index.js
@@ -18,8 +18,49 @@ bot.help((ctx) => {
   );
 });
 
+//Translate uzbek-english command
 
+bot.command("tr_uz", async (ctx) => {
+  const text = ctx.message.text?.replace("/tr_uz", "")?.trim().toLowerCase();
 
+  if (text) {
+    ctx.sendChatAction("typing");
+    // Call a translation API or implement your own translation logic here
+    const translatedText = await translateUzbekToEnglish(text);
+    ctx.telegram.sendMessage(ctx.message.chat.id, translatedText, {
+      reply_to_message_id: ctx.message.message_id,
+    });
+  } else {
+    ctx.telegram.sendMessage(
+      ctx.message.chat.id,
+      "Please provide text to translate after /tr_uz",
+      {
+        reply_to_message_id: ctx.message.message_id,
+      }
+    );
+  }
+});
+
+bot.command("tr_eng", async (ctx) => {
+  const text = ctx.message.text?.replace("/tr_eng", "")?.trim().toLowerCase();
+
+  if (text) {
+    ctx.sendChatAction("typing");
+    // Call a translation API or implement your own translation logic here
+    const translatedText = await translateEnglishToUzbek(text);
+    ctx.telegram.sendMessage(ctx.message.chat.id, translatedText, {
+      reply_to_message_id: ctx.message.message_id,
+    });
+  } else {
+    ctx.telegram.sendMessage(
+      ctx.message.chat.id,
+      "Please provide text to translate after /tr_eng",
+      {
+        reply_to_message_id: ctx.message.message_id,
+      }
+    );
+  }
+});
 
 
 
