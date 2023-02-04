ba0ff39ef49c5133592ee604d81eb6818b048c1e
Arifmetik <100046368+Arifmetik@users.noreply.github.com>
2023-02-04 19:15:37 +0500

    Update index.js

--- index.js
+++ index.js
@@ -18,51 +18,51 @@ const bot = new Telegraf(process.env.TG_API);
 bot.start((ctx) => ctx.reply("Welcome , You can ask anything from me"));
 
 if (connectionString && connectionString.startsWith('mongodb://')) {
-let db;
+  let db;
 
-MongoClient.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
-  if (error) {
-    console.log("Error connecting to MongoDB:", error);
-  } else {
-    console.log("Successfully connected to MongoDB");
-    db = client.db("telegram-bot-db");
-  }
-});
-}
+  MongoClient.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
+    if (error) {
+      console.log("Error connecting to MongoDB:", error);
+    } else {
+      console.log("Successfully connected to MongoDB");
+      db = client.db("telegram-bot-db");
+    }
+  });
+  
+  bot.command("allow", (ctx) => {
+    if (db) {
+      if (ctx.from.id.toString() === botOwnerId) {
+        const userId = ctx.message.text.split(" ")[1].replace("/", "");
+        db.collection("allowed_users").insertOne({ user_id: userId }, (error, result) => {
+          if (error) {
+            console.log("Error adding user to allowed_users collection:", error);
+          } else {
+            ctx.reply(`User with ID ${userId} has been granted access to /ask command`);
+          }
+        });
+      } else {
+        ctx.reply("You are not the bot owner");
+      }
+    } else {
+      ctx.reply("Error connecting to MongoDB, cannot perform this action");
+    }
+  });
 
-bot.command("allow", (ctx) => {
-  if (db) {
-    if (ctx.from.id.toString() === botOwnerId) {
-      const userId = ctx.message.text.split(" ")[1].replace("/", "");
-      db.collection("allowed_users").insertOne({ user_id: userId }, (error, result) => {
+  bot.command("ask", (ctx) => {
+    if (db) {
+      db.collection("allowed_users").findOne({ user_id: ctx.from.id.toString() }, (error, result) => {
         if (error) {
-          console.log("Error adding user to allowed_users collection:", error);
+          console.log("Error searching for user in allowed_users collection:", error);
+        } else if (result) {
+          // logic for handling the /ask command
         } else {
-          ctx.reply(`User with ID ${userId} has been granted access to /ask command`);
+          ctx.reply("You do not have permission to use the /ask command");
         }
       });
     } else {
-      ctx.reply("You are not the bot owner");
+      ctx.reply("Error connecting to MongoDB, cannot perform this action");
     }
-  } else {
-    ctx.reply("Error connecting to MongoDB, cannot perform this action");
-  }
-});
-
-bot.command("ask", (ctx) => {
-  if (db) {
-    db.collection("allowed_users").findOne({ user_id: ctx.from.id.toString() }, (error, result) => {
-      if (error) {
-        console.log("Error searching for user in allowed_users collection:", error);
-      } else if (result) {
-        // logic for handling the /ask command
-      } else {
-        ctx.reply("You do not have permission to use the /ask command");
-      }
-    });
-  } else {
-    ctx.reply("Error connecting to MongoDB, cannot perform this action");
-  }
-});
+  });
+}
 
 bot.launch();
