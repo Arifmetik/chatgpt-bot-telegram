f5cf6f1c708ce33b137be32f79383ffc79c92610
Arifmetik <100046368+Arifmetik@users.noreply.github.com>
2023-02-04 13:14:58 +0500

    Update index.js

--- index.js
+++ index.js
@@ -54,6 +54,22 @@ bot.command('connectdb', (ctx) => {
   }
 });
 
+const mongoose = require("mongoose");
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+
+db.on("error", (error) => {
+  console.error(`Error connecting to the database: ${error}`);
+});
+
+db.once("open", () => {
+  console.log("Connected to the database.");
+});
 
 
 // Chat command
