d0940c081a6354cce4b2ac1610a838e2f58df624
Arifmetik <100046368+Arifmetik@users.noreply.github.com>
2023-02-04 16:48:29 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,48 +1,46 @@
-const Telegraf = require('telegraf');
-const mongoose = require('mongoose');
-const { reply } = require('telegraf/command-parts');
-require('dotenv').config();
+const Telegraf = require('telegraf')
+const { MongoClient } = require('mongodb')
+const bot = new Telegraf(process.env.TG_API)
 
-const bot = new Telegraf(process.env.TG_API);
+const uri = process.env.MONGO_URI
+const botOwnerId = process.env.BOT_OWNER_ID
 
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('MongoDB connected'))
-  .catch(error => console.error(error));
+let db
 
-const allowedUserSchema = new mongoose.Schema({
-  userId: {
-    type: Number,
-    required: true,
-    unique: true,
-  },
-});
-
-const AllowedUser = mongoose.model('AllowedUser', allowedUserSchema);
-
-bot.command('allow', async (ctx) => {
-  const userId = ctx.message.text.split(' ')[1];
-  if (!userId) {
-    return ctx.reply('Please provide a valid user ID after the /allow command.');
+MongoClient.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
+  if (error) {
+    console.log(error)
+  } else {
+    console.log('Connected to MongoDB')
+    db = client.db('telegram-bot-db')
   }
+})
 
-  try {
-    const allowedUser = new AllowedUser({ userId });
-    await allowedUser.save();
-    ctx.reply(`User with ID ${userId} has been granted permission to use the /ask command.`);
-  } catch (error) {
-    ctx.reply('Error while granting permission. Please try again.');
+bot.command('allow', (ctx) => {
+  if (ctx.from.id.toString() === botOwnerId) {
+    const userId = ctx.message.text.split(' ')[1].replace('/', '')
+    db.collection('allowed_users').insertOne({ user_id: userId }, (error, result) => {
+      if (error) {
+        console.log(error)
+      } else {
+        ctx.reply(`User with ID ${userId} has been granted access to /ask command`)
+      }
+    })
+  } else {
+    ctx.reply('You are not the bot owner')
   }
-});
+})
 
-bot.command('ask', async (ctx) => {
-  const userId = ctx.from.id;
-  const isAllowed = await AllowedUser.findOne({ userId });
+bot.command('ask', (ctx) => {
+  db.collection('allowed_users').findOne({ user_id: ctx.from.id.toString() }, (error, result) => {
+    if (error) {
+      console.log(error)
+    } else if (result) {
+      // logic for handling the /ask command
+    } else {
+      ctx.reply('You do not have permission to use the /ask command')
+    }
+  })
+})
 
-  if (!isAllowed) {
-    return ctx.reply('You are not allowed to use the /ask command. Please contact the bot owner for permission.');
-  }
-
-  // Your code to handle the /ask command here
-});
-
-bot.launch();
+bot.launch()
