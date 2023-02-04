787c83172ad5d669e9b3646b62417f1b4f409451
Arifmetik <100046368+Arifmetik@users.noreply.github.com>
2023-02-04 18:46:26 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-require('dotenv').config()
-const { MongoClient } = require('mongodb')
+const { MongoClient } = require('mongodb');
 const mongoClient = require("mongodb").MongoClient;
 const uri = process.env.MONGODB_URI;
 const botOwnerId = process.env.BOT_OWNER_ID;
 
-
+require('dotenv').config();
 const { Configuration, OpenAIApi } = require("openai");
 const { getImage, getChat } = require("./Helper/functions");
 const { Telegraf } = require("telegraf");
@@ -18,42 +17,50 @@ module.exports = openai;
 const bot = new Telegraf(process.env.TG_API);
 bot.start((ctx) => ctx.reply("Welcome , You can ask anything from me"));
 
-let db
+let db;
 
 MongoClient.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
   if (error) {
-    console.log(error)
+    console.log("Error connecting to MongoDB:", error);
   } else {
-    console.log('Connected to MongoDB')
-    db = client.db('telegram-bot-db')
+    console.log("Successfully connected to MongoDB");
+    db = client.db("telegram-bot-db");
   }
-})
+});
 
-bot.command('allow', (ctx) => {
-  if (ctx.from.id.toString() === botOwnerId) {
-    const userId = ctx.message.text.split(' ')[1].replace('/', '')
-    db.collection('allowed_users').insertOne({ user_id: userId }, (error, result) => {
-      if (error) {
-        console.log(error)
-      } else {
-        ctx.reply(`User with ID ${userId} has been granted access to /ask command`)
-      }
-    })
-  } else {
-    ctx.reply('You are not the bot owner')
-  }
-})
-
-bot.command('ask', (ctx) => {
-  db.collection('allowed_users').findOne({ user_id: ctx.from.id.toString() }, (error, result) => {
-    if (error) {
-      console.log(error)
-    } else if (result) {
-      // logic for handling the /ask command
+bot.command("allow", (ctx) => {
+  if (db) {
+    if (ctx.from.id.toString() === botOwnerId) {
+      const userId = ctx.message.text.split(" ")[1].replace("/", "");
+      db.collection("allowed_users").insertOne({ user_id: userId }, (error, result) => {
+        if (error) {
+          console.log("Error adding user to allowed_users collection:", error);
+        } else {
+          ctx.reply(`User with ID ${userId} has been granted access to /ask command`);
+        }
+      });
     } else {
-      ctx.reply('You do not have permission to use the /ask command')
+      ctx.reply("You are not the bot owner");
     }
-  })
-})
+  } else {
+    ctx.reply("Error connecting to MongoDB, cannot perform this action");
+  }
+});
 
-bot.launch()
+bot.command("ask", (ctx) => {
+  if (db) {
+    db.collection("allowed_users").findOne({ user_id: ctx.from.id.toString() }, (error, result) => {
+      if (error) {
+        console.log("Error searching for user in allowed_users collection:", error);
+      } else if (result) {
+        // logic for handling the /ask command
+      } else {
+        ctx.reply("You do not have permission to use the /ask command");
+      }
+    });
+  } else {
+    ctx.reply("Error connecting to MongoDB, cannot perform this action");
+  }
+});
+
+bot.launch();
