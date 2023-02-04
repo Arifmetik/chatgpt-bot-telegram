79a6a2c4f5ef4db7b9660173b01773369ecb3805
Arifmetik <100046368+Arifmetik@users.noreply.github.com>
2023-02-04 13:30:13 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,106 +1,48 @@
-require('dotenv').config()
-const { Configuration, OpenAIApi } = require("openai");
-const { getImage, getChat } = require("./Helper/functions");
-const { Telegraf } = require("telegraf");
-
-const configuration = new Configuration({
-  apiKey: process.env.API,
-});
-const openai = new OpenAIApi(configuration);
-module.exports = openai;
+const Telegraf = require('telegraf');
+const mongoose = require('mongoose');
+const { reply } = require('telegraf/command-parts');
+require('dotenv').config();
 
 const bot = new Telegraf(process.env.TG_API);
-bot.start((ctx) => ctx.reply("Welcome , You can ask anything from me"));
 
-bot.help((ctx) => {
-  ctx.reply(
-    "This bot can perform the following command \n /image -> to create image from text \n /ask -> ank anything from me "
-  );
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected'))
+  .catch(error => console.error(error));
+
+const allowedUserSchema = new mongoose.Schema({
+  userId: {
+    type: Number,
+    required: true,
+    unique: true,
+  },
 });
 
-//Test translate
-const translate = require('translate-google');
+const AllowedUser = mongoose.model('AllowedUser', allowedUserSchema);
 
+bot.command('allow', async (ctx) => {
+  const userId = ctx.message.text.split(' ')[1];
+  if (!userId) {
+    return ctx.reply('Please provide a valid user ID after the /allow command.');
+  }
 
-bot.command('eng_uz', async ctx => {
-  const text = ctx.message.text.split(' ').slice(1).join(' ');
   try {
-    const translation = await translate(text, { to: 'uz' });
-    ctx.reply(translation);
+    const allowedUser = new AllowedUser({ userId });
+    await allowedUser.save();
+    ctx.reply(`User with ID ${userId} has been granted permission to use the /ask command.`);
   } catch (error) {
-    ctx.reply('Try again');
+    ctx.reply('Error while granting permission. Please try again.');
   }
 });
 
-bot.command('uz_eng', async ctx => {
-  const text = ctx.message.text.split(' ').slice(1).join(' ');
-  try {
-    const translation = await translate(text, { to: 'en' });
-    ctx.reply(translation);
-  } catch (error) {
-    ctx.reply('Try again');
+bot.command('ask', async (ctx) => {
+  const userId = ctx.from.id;
+  const isAllowed = await AllowedUser.findOne({ userId });
+
+  if (!isAllowed) {
+    return ctx.reply('You are not allowed to use the /ask command. Please contact the bot owner for permission.');
   }
-  });
 
-
-// Test commands
-
-bot.command('connectdb', (ctx) => {
-  // Connect to the database here and check if the connection was successful
-  if (successful) {
-    ctx.reply('Connected');
-  } else {
-    ctx.reply('Error');
-  }
+  // Your code to handle the /ask command here
 });
-
-const mongoose = require("mongoose");
-
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
-
-const db = mongoose.connection;
-
-db.on("error", (error) => {
-  console.error(`Error connecting to the database: ${error}`);
-});
-
-db.once("open", () => {
-  console.log("Connected to the database.");
-});
-
-
-// Chat command
-
-bot.command("ask", async (ctx) => {
-  const text = ctx.message.text?.replace("/ask", "")?.trim().toLowerCase();
-
-const axios = require("axios");
-
-
-  if (text) {
-    ctx.sendChatAction("typing");
-    const res = await getChat(text);
-    if (res) {
-      ctx.telegram.sendMessage(ctx.message.chat.id, res, {
-        reply_to_message_id: ctx.message.message_id,
-      });
-    }
-  } else {
-    ctx.telegram.sendMessage(
-      ctx.message.chat.id,
-      "Please ask anything after /ask",
-      {
-        reply_to_message_id: ctx.message.message_id,
-      }
-    );
-  
-    //  reply("Please ask anything after /ask");
-  }
-});
-
-
 
 bot.launch();
